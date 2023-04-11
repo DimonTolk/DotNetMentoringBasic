1ba29f8119985e132fba82a169f7a3380b26c94f
DimonTolk <dimontolk.1996@gmail.com>
2023-04-11 15:58:20 +0300

    Implemented task

---
+++ Module 13 ADO.NET/Coding task/ADOLibrary/Database/obj/Debug/postdeploy.sql
@@ -0,0 +1,7 @@
+﻿insert into [Product] ([Weight], [Height], [Width], [Length], [Name], [Description]) values (1, 1, 1, 1, 'test', 'test');
+insert into [Product] ([Weight], [Height], [Width], [Length], [Name], [Description]) values (1, 1, 1, 1, 'test', 'test2');
+
+insert into [Order] ([Status], [CreatedDate], [UpdatedDate], [ProductId]) values ('Done', '2022-01-01', '2022-01-01', 1);
+insert into [Order] ([Status], [CreatedDate], [UpdatedDate], [ProductId]) values ('Done', '2022-01-01', '2022-01-01', 1);
+insert into [Order] ([Status], [CreatedDate], [UpdatedDate], [ProductId]) values ('Done', '2022-01-01', '2022-01-01', 1);
+GO
---
+++ Module 8 SQL Fundamentals/Coding task/Database1/Database1/obj/Debug/postdeploy.sql
@@ -0,0 +1,14 @@
+﻿insert into [Address] ([Street], [City], [State], [ZipCode]) values ('Partizansky avenue, 32', 'Minsk', 'Belarus','220000');
+insert into [Address] ([Street], [City], [State], [ZipCode]) values ('Riddle Mansion', 'Little Hangleton', 'Great Britain','000000');
+insert into [Address] ([Street], [City], [State], [ZipCode]) values ('1/1 Kuprevicha','Minsk','Belarus','220141');
+insert into [Address] ([Street], [City], [State], [ZipCode]) values ('Hogwarts Castle','Scottish Highlands','Great Britain','220141');
+
+insert into [Person] ([FirstName], [LastName]) values ('Dmitry','Tolkachev');
+insert into [Person] ([FirstName], [LastName]) values ('Tom','Riddle');
+
+insert into [Company] ([Name], [AddressId]) values ('EPAM', 3);
+insert into [Company] ([Name], [AddressId]) values ('Hogwarts', 4);
+
+insert into [Employee] ([AddressId], [PersonId], [CompanyName], [Position]) values (1, 1, 'EPAM', 'Junior Software Developer');
+insert into [Employee] ([AddressId], [PersonId], [CompanyName], [Position], [EmployeeName]) values ('2','2','Hogwarts','Dark Lord', 'Lord Voldemort');
+GO
